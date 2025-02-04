92b57296fde929994337f64a66fbb4386ba6d37c
Gray Lloyd <362572141@GoTVDSB.ca>
2025-02-04 14:45:20 -0500

    Finish

--- Program.cs
+++ Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Initials of Gray Lloyd
+            Console.WriteLine("Hello Mr. Aldworth, I'm Gray");
+            Console.WriteLine();
+            Console.WriteLine("Comments are used as separations from code to explain things");
+
+            Console.WriteLine();
+
+            Console.WriteLine(" GGG  L");
+            Console.WriteLine("G   G L");
+            Console.WriteLine("G     L");
+            Console.WriteLine("GGGGG L");
+            Console.WriteLine("G   G L");
+            Console.WriteLine("G   G L");
+            Console.WriteLine(" GGG  LLLLLLL");
+            //This is code
         }
     }
 }
